80363548e77b774855e90b2d7a0269faa7e5fcb7
LOVETESH <luvteshchandra@gmail.com>
2019-09-08 00:20:49 +0530

    added android files

---
+++ Android/Syndicatefinal/app/src/main/java/me/kumarrohit/syndicatefinal/previous_request.java
@@ -0,0 +1,31 @@
+package me.kumarrohit.syndicatefinal;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class previous_request extends Fragment {
+
+
+    public previous_request() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_previous_request, container, false);
+    }
+
+}
